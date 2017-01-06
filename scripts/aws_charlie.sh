62271916db0ee127d8d50ffcf79bd496e570347c
Boyoung- <byaweics@gmail.com>
2017-01-05 16:17:16 -0800

    update scripts

--- scripts/aws_charlie.sh
+++ scripts/aws_charlie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip 35.161.54.54 -debbie_ip 35.162.185.55 charlie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol run -eddie_ip 35.162.230.27 -debbie_ip 35.163.125.170 charlie
--- scripts/aws_debbie.sh
+++ scripts/aws_debbie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip 35.161.54.54 debbie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol run -eddie_ip 35.162.230.27 debbie
