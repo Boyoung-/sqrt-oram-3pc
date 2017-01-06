d8ebb7c1027674d8172848b2da5ce75d31315747
Boyoung- <byaweics@gmail.com>
2017-01-06 15:48:26 -0800

    update aws ips

--- scripts/aws_charlie.sh
+++ scripts/aws_charlie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol run -eddie_ip 35.162.230.27 -debbie_ip 35.163.125.170 charlie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol run -eddie_ip 35.164.214.188 -debbie_ip 35.164.214.188 charlie
--- scripts/aws_debbie.sh
+++ scripts/aws_debbie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol run -eddie_ip 35.162.230.27 debbie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol run -eddie_ip 35.164.214.188 debbie
